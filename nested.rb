ebf9efd1e8bd9c5c169563b72d67744b9da8e65c
Matt Fender <mjfender@gmail.com>
2016-11-22 02:11:26 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -109,7 +109,7 @@ def changing_alan
      #change what Alan Kay is :known_for the value of the alans_new_info variable.
 		 programmer_hash[:alan_kay][:known_for] = "GUI"
 		 puts programmer_hash[:alan_kay][:known_for]
-		 programmer_hash[:alan_kay][:known_for]
+		 programmer_hash
 end
 
 def adding_to_dennis
@@ -132,7 +132,6 @@ dennis_languages = []
 dennis_languages = programmer_hash[:dennis_ritchie][:languages]
 dennis_languages.push("Assembly")
 programmer_hash[:dennis_ritchie][:languages] = dennis_languages
-puts dennis_languages
 puts programmer_hash[:dennis_ritchie][:languages]
-programmer_hash[:dennis_ritchie][:languages]
+programmer_hash
 end
